033527869e3cf6aca58ec6ad6541c1653befd039
Seema Taneja <seemamehta001@gmail.com>
2022-05-04 14:11:29 +0100

    available moves on pawn

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -6,13 +6,20 @@ export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
-
+//Handle only first move
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
+        if (this.player === Player.WHITE && location.row === 1) {
+            return[Square.at(location.row + 1, location.col),
+                   Square.at(location.row + 2, location.col)];
+           } else if (this.player === Player.BLACK && location.row === 6){
+            return [Square.at(location.row - 1, location.col),
+                Square.at(location.row - 2, location.col)];
+        }
+        if(this.player === Player.WHITE){
+           return [Square.at(location.row + 1, location.col)];
+        }else if(this.player === Player.BLACK){
+            return [Square.at(location.row - 1, location.col)];
         }
     }
 }
