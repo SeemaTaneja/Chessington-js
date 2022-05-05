0b3c95ae0dd14805c36b81761aa5537d2b76f3e8
Seema Taneja <seemamehta001@gmail.com>
2022-05-05 15:37:23 +0100

    passed queen move

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,42 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let newMovesArray = [];
+        for(let i= 0 ; i<= 7 ; i++){
+            if(i !== location.row){
+                newMovesArray.push(Square.at(i , location.col));
+            }       
+        }
+        for(let i= 0 ; i<= 7 ; i++){
+            if(i !== location.col){
+                newMovesArray.push(Square.at(location.row , i));
+            }
+        }
+        for(let i = 0 ; i <= 7 -location.col  ; i++){
+            const nextLocation = Square.at(location.row + i , location.col + i);
+            if(!nextLocation.equals(location)){
+             newMovesArray.push(nextLocation);
+            }
+        }
+        for(let i = 0 ; i <= location.col ; i++){
+            const nextLocation = Square.at(location.row + i , location.col - i);
+            if(!nextLocation.equals(location)){
+             newMovesArray.push(nextLocation);
+            }
+        }
+        for(let i = 0 ; i <= location.row ; i++){
+            const nextLocation = Square.at(location.row - i , location.col + i);
+            if(!nextLocation.equals(location)){
+            newMovesArray.push(nextLocation);
+        }
+    }
+    for(let i = 0 ; i <= location.row ; i++){
+        const nextLocation = Square.at(location.row - i , location.col - i);
+        if(!nextLocation.equals(location)){
+        newMovesArray.push(nextLocation);
+    }
+    }
+        return newMovesArray;
     }
 }
