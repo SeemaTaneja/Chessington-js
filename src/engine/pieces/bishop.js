160fbd534455badd457a4e8ba8133b3a65625b85
Seema Taneja <seemamehta001@gmail.com>
2022-05-05 14:34:03 +0100

    bishop move test completed

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,38 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let newMovesArray = [];
+        console.log("Starting location is :", location);
+//1st quardarant(forward right)
+       for(let i = 0 ; i <= 7 -location.col  ; i++){
+           const nextLocation = Square.at(location.row + i , location.col + i);
+           if(!nextLocation.equals(location)){
+            newMovesArray.push(nextLocation);
+           }
+       }
+ //2nd quardarant( forward left)   
+       for(let i = 0 ; i <= location.col ; i++){
+           const nextLocation = Square.at(location.row + i , location.col - i);
+           if(!nextLocation.equals(location)){
+            newMovesArray.push(nextLocation);
+           }
+       }
+ //3rd quararant(Backward right) 
+        for(let i = 0 ; i <= location.row ; i++){
+            const nextLocation = Square.at(location.row - i , location.col + i);
+        if(!nextLocation.equals(location)){
+        newMovesArray.push(nextLocation);
+        }
     }
+  //4th quardarant (backward left)
+         for(let i = 0 ; i <= location.row ; i++){
+             const nextLocation = Square.at(location.row - i , location.col - i);
+         if(!nextLocation.equals(location)){
+          newMovesArray.push(nextLocation);
+    }
+} 
+        console.log(newMovesArray);
+        return newMovesArray;
+}
 }
