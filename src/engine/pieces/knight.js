6695691cd2f939cd890155ccbf05934e682abe88
Seema Taneja <seemamehta001@gmail.com>
2022-05-09 21:16:14 +0100

    knight move

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,23 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let newMovesArray = [];
+        console.log("Starting location is :", location);
+        //first move forward right
+        let i =location.row;
+        let j = location.col;
+        if(i >= 0 && i < 8 && j >= 0 && j < 8){
+             newMovesArray.push(Square.at( i = location.row + 1 , j = location.col + 2)),
+             newMovesArray.push(Square.at( i = location.row - 1 , j = location.col + 2)),
+             newMovesArray.push(Square.at(i = location.row + 2 , j = location.col + 1 )),
+             newMovesArray.push(Square.at(i = location.row + 2 , j = location.col - 1 )),
+             newMovesArray.push(Square.at( i = location.row - 2 , j = location.col + 1)),
+             newMovesArray.push(Square.at( i = location.row - 2 , j = location.col - 1)),
+             newMovesArray.push(Square.at(i = location.row - 1 , j = location.col - 2 )),
+             newMovesArray.push(Square.at(i = location.row + 1 , j = location.col - 2 ))
+        }
+        console.log(newMovesArray);
+        return newMovesArray;
     }
 }
