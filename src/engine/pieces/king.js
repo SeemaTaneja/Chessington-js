dc05e3bdd70fe820d0afe6dae0083d7ab606b171
Seema Taneja <seemamehta001@gmail.com>
2022-05-10 18:05:58 +0100

    king move

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,20 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let newMovesArray = [];
+        let i =location.row;
+        let j = location.col;
+        if(i >= 0 && i < 8 && j >= 0 && j < 8){
+            newMovesArray.push(Square.at( i = location.row + 1 , j = location.col)),
+             newMovesArray.push(Square.at( i = location.row - 1 , j = location.col)),
+             newMovesArray.push(Square.at(i = location.row , j = location.col + 1 )),
+             newMovesArray.push(Square.at(i = location.row , j = location.col - 1 )),
+             newMovesArray.push(Square.at( i = location.row + 1 , j = location.col + 1)),
+             newMovesArray.push(Square.at( i = location.row + 1 , j = location.col - 1)),
+             newMovesArray.push(Square.at(i = location.row - 1 , j = location.col - 1 )),
+             newMovesArray.push(Square.at(i = location.row - 1 , j = location.col + 1 ))
+        }
+        return newMovesArray;
     }
 }
